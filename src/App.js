717b01e43db5ecfbed6f1ac4eea6f89aa3c3a65f
strijunk <izzulkhaq75@gmail.com>
2021-11-24 11:07:37 +0700

    adding pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, NavLink, Routes} from "react-router-dom";
-import { GrGamepad, GrUser } from "react-icons/gr";
+import { GrGamepad, GrUser, GrGift } from "react-icons/gr";
 
 import Detail from "./pages/Detail.js"
 import "./App.css";
 import Game from "./pages/Game";
 import Profile from "./pages/Profile";
+import Giveaway from "./pages/Giveaway.js";
+import Gadetail from "./pages/Ga-detail.js";
 
 function App() {
   return (
@@ -17,10 +19,13 @@ function App() {
                       <Routes>
                         <Route path="/" element={<Game/>}></Route>
                         <Route path="/Detail/:id" element={<Detail/>}></Route>
+                        <Route path="/Ga-detail/:id" element={<Gadetail/>}></Route>
                         <Route path="/Profile" element={<Profile/>}></Route>
+                        <Route path="/Giveaway" element={<Giveaway/>}></Route>
                       </Routes>
                 <footer>
                         <NavLink to="/" className="iconwrapper"><GrGamepad className="icon"/>Game</NavLink>
+                        <NavLink to="/Giveaway" className="iconwrapper"><GrGift className="icon"/>Giveaway</NavLink>
                         <NavLink to="/Profile" className="iconwrapper"><GrUser className="icon"/>Profile</NavLink>
                 </footer>
             </BrowserRouter>
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,5 +1,3 @@
-import React, {useState, useEffect} from "react";
-import axios from "axios";
 import CardProfile from "../component/CardProfile";
 
 function Profile(){
